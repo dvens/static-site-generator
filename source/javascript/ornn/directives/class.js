f0ada725ac6af63a4fbf50791b53f8877c43117b
dvens <d.joelvens@gmail.com>
2018-04-16 22:31:00 +0200

    class cleanup

--- source/javascript/ornn/directives/class.js
+++ source/javascript/ornn/directives/class.js
@@ -23,29 +23,3 @@ export default {
     }
 
 };
-
-
-// const _class = {
-
-//     update: ( cl ) => {
-
-//         each( cl, ( item, key ) => {
-
-//             if( typeOf( item ) === 'Object' ) {
-
-//                 each( item, ( value, c ) => value ? addClass( this.node, c ) : removeClass( this.node, c ) );
-
-//             } else {
-
-//                 const className = typeOf( key ) === 'Number' ? item : i;
-//                 item ? addClass(this.node, className) : removeClass(this.node, className);
-
-//             }
-
-//         });
-
-//     }
-
-// };
-
-// export default _class;
