896300b40990e9eca4bb9940ccec2ca886edeb46
dylan vens <dylan.vens@tamtam.nl>
2018-04-27 14:35:04 +0200

    added ornn v0.9

--- source/javascript/main.js
+++ source/javascript/main.js
@@ -1,59 +1,59 @@
-'use strict';
+// 'use strict';
 
-import Ornn from './ornn';
-export default class main {
+// import Ornn from './ornn';
+// export default class main {
 
-    /**
-     * Launches the modules
-     * @method init
-     * @static
-     */
-    static init() {
+//     /**
+//      * Launches the modules
+//      * @method init
+//      * @static
+//      */
+//     static init() {
 
 
-        const data = {
-            isActive: true,
-            isFixed: true,
-            activeColor: '#42b983',
-            fontSize: 16,
-            styleObject1: {
-                color: '#42b983',
-                backgroundSize: 'cover',
-            },
-            styleObject2: {
-                fontSize: 16 + 'px',
-            },
-            fullName: null,
-            gasValue: false,
-            test: null,
-            user: {
-                avatar: `./images/avatar.jpg`,
-                firstName: `Just`,
-                lastName: `Me`,
-            },
-            moments: [{
-                content: `Hello World.`,
-                show: true,
-            }, {
-                content: `Hello World.`,
-                show: false,
-            }, {
-                content: `Hello World.`,
-                show: true,
-            }],
-        };
+//         const data = {
+//             isActive: true,
+//             isFixed: true,
+//             activeColor: '#42b983',
+//             fontSize: 16,
+//             styleObject1: {
+//                 color: '#42b983',
+//                 backgroundSize: 'cover',
+//             },
+//             styleObject2: {
+//                 fontSize: 16 + 'px',
+//             },
+//             fullName: null,
+//             gasValue: false,
+//             test: null,
+//             user: {
+//                 avatar: `./images/avatar.jpg`,
+//                 firstName: `Just`,
+//                 lastName: `Me`,
+//             },
+//             moments: [{
+//                 content: `Hello World.`,
+//                 show: true,
+//             }, {
+//                 content: `Hello World.`,
+//                 show: false,
+//             }, {
+//                 content: `Hello World.`,
+//                 show: true,
+//             }],
+//         };
 
-            const products = new Ornn({
-                el: document.querySelector( '.app' ),
-                data,
-                computed: {
-                    fullName() {
-                        return this.user.firstName + this.user.lastName;
-                    }
-                }
-            });
-    }
+//             const products = new Ornn({
+//                 el: document.querySelector( '.app' ),
+//                 data,
+//                 computed: {
+//                     fullName() {
+//                         return this.user.firstName + this.user.lastName;
+//                     }
+//                 }
+//             });
+//     }
 
-}
+// }
 
-main.init();
+// main.init();
